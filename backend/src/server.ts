393a368bc463944d1cd9f137f3694ffc6530affd
ATN <contact.atn35@gmail.com>
2025-06-17 23:12:35 +0200

    add authorized origins to server.ts file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,9 +17,11 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "http://49.12.238.138:3000",
+  "http://49.12.238.138:4000",
   "http://carnetdelecture.duckdns.org",
   "http://carnetdelecture.duckdns.org:3000",
   "http://carnetdelecture.duckdns.org:4000",
+  "https://carnetdelecture.duckdns.org",
 ];
 
 app.use(
